f61461687fc5faf71accfadfdb57be633c1ef766
Teguh-Mulya <teguhmulya1205@gmail.com>
2024-09-13 19:07:54 +0700

    update cetak jadwal dan ganti jadwal

--- penyiar-jadwal/database/seeders/UserSeeder.php
+++ penyiar-jadwal/database/seeders/UserSeeder.php
@@ -15,19 +15,16 @@ class UserSeeder extends Seeder
         $users = [
 
             ['username' => 'user1', 'name' => 'Widi Kurniawan', 'email' => 'widi.kur@example.com', 'password' => 'password', 'roles' => ['Kepala Stasiun'], 'status' => 'active'],
-            ['username' => 'user2', 'name' => 'Budi Santoso', 'email' => 'budi.santoso@example.com', 'password' => 'password', 'roles' => ['Kepala Bidang Siaran'], 'status' => 'inactive'],
-            ['username' => 'user3', 'name' => 'Citra Dewi', 'email' => 'citra.dewi@example.com', 'password' => 'password', 'roles' => ['Koordinator Siaran'], 'status' => 'active'],
-            ['username' => 'user4', 'name' => 'Dani Putra', 'email' => 'dani.putra@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'inactive'],
-            ['username' => 'user5', 'name' => 'Eka Wulandari', 'email' => 'eka.wulandari@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'active'],
-            ['username' => 'user6', 'name' => 'Feri Hadi', 'email' => 'feri.hadi@example.com', 'password' => 'password', 'roles' => [ 'Penyiar'], 'status' => 'active'],
-            ['username' => 'user7', 'name' => 'Gina Oktavia', 'email' => 'gina.oktavia@example.com', 'password' => 'password', 'roles' => [ 'Penyiar'], 'status' => 'inactive'],
-            ['username' => 'user8', 'name' => 'Hendra Kusuma', 'email' => 'hendra.kusuma@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'active'],
-            ['username' => 'user9', 'name' => 'Indah Sari', 'email' => 'indah.sari@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'inactive'],
-            ['username' => 'user10', 'name' => 'Joko Prabowo', 'email' => 'joko.prabowo@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'active'],
-            ['username' => 'user11', 'name' => 'Krisna Wijaya', 'email' => 'krisna.wijaya@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'inactive'],
-            ['username' => 'user12', 'name' => 'Lina Sari', 'email' => 'lina.sari@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'active'],
-            ['username' => 'user13', 'name' => 'Maya Anggraini', 'email' => 'maya.anggraini@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'active'],
-            ['username' => 'user14', 'name' => 'Nina Fitria', 'email' => 'nina.fitria@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'inactive'],
+            ['username' => 'user2', 'name' => 'Dudi', 'email' => 'dudi@example.com', 'password' => 'password', 'roles' => ['Kepala Bidang Siaran'], 'status' => 'inactive'],
+            ['username' => 'user3', 'name' => 'Zaki', 'email' => 'zaki@example.com', 'password' => 'password', 'roles' => ['Koordinator Siaran'], 'status' => 'active'],
+            ['username' => 'user4', 'name' => 'Uli Tanuwijaya', 'email' => 'uli@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'inactive'],
+            ['username' => 'user5', 'name' => 'Raya Alifa', 'email' => 'alifa.raya@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'active'],
+            ['username' => 'user6', 'name' => 'Winda Anindita', 'email' => 'anindita.winda@example.com', 'password' => 'password', 'roles' => [ 'Penyiar'], 'status' => 'active'],
+            ['username' => 'user7', 'name' => 'Randy Aditya', 'email' => 'aditya.randy@example.com', 'password' => 'password', 'roles' => [ 'Penyiar'], 'status' => 'inactive'],
+            ['username' => 'user8', 'name' => 'Rangga Hadiansyah', 'email' => 'hadiansyah.rangga@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'active'],
+            ['username' => 'user9', 'name' => 'Sendy Puspa Andini', 'email' => 'andini.sendy@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'inactive'],
+            ['username' => 'user10', 'name' => 'Rio Risdianto', 'email' => 'kobe.rio@example.com', 'password' => 'password', 'roles' => ['Penyiar'], 'status' => 'active'],
+           
         ];
 
         // Insert user data
